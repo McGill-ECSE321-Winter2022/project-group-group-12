550782b93196718517a6c0f9d4981c3b4944d42e
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-02-11 19:16:15 -0500

    First commit

---
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/GsssBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.GSSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsssBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsssBackendApplication.class, args);
+	}
+
+}
