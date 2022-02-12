aa77416d31edadf571942d78d9545e590e071923
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-02-11 19:34:51 -0500

    Minor backend fixes

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/GsssBackendApplication.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/GsssBackendApplication.java
@@ -1,13 +1,21 @@
 package ca.mcgill.ecse321.GSSS;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class GsssBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GsssBackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GsssBackendApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 
 }
