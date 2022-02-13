1f738cb8163ecd264a9e0ea23c0d8a1db8f71526
HabibJarweh <89218041+HabibJarweh@users.noreply.github.com>
2022-02-13 11:55:38 -0500

    added java doc

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/OrderType.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/OrderType.java
@@ -1,5 +1,11 @@
 package ca.mcgill.ecse321.GSSS.model;
 
+/**
+ * @author habib jarweh
+ *
+ *enumeration OrderType, used by the Order class to define the order type, 
+ *since we have two types of orders possible on the website
+ */
 public enum OrderType {
 	delivery, 
 	pickup
--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/Weekday.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/Weekday.java
@@ -1,5 +1,11 @@
 package ca.mcgill.ecse321.GSSS.model;
 
+/**
+ * @author Habib Jarweh
+ *
+ *enumeration Weekday, used by the BusinessHour class
+ *this is to define opening hours on a specific day of the week
+ */
 public enum Weekday {
 
 	Monday,
