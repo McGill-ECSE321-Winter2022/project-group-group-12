ec9e45e5cf2eb895cd97e87c747a6bf48447da8a
HabibJarweh <89218041+HabibJarweh@users.noreply.github.com>
2022-02-13 18:25:56 -0500

    edited java doc

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/OrderType.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/OrderType.java
@@ -7,8 +7,6 @@ package ca.mcgill.ecse321.GSSS.model;
  *
  * @author habib jarweh
  *
- *         enumeration OrderType, used by the Order class to define the order type, since we have
- *         two types of orders possible on the website
  */
 public enum OrderType {
   delivery, pickup
