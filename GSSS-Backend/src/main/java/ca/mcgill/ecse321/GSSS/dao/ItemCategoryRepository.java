18323ba22391a9e45117d65619bbd061c1f75f1c
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2022-02-12 21:57:50 -0500

    ItemCategory Repository implementation

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/dao/ItemCategoryRepository.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/dao/ItemCategoryRepository.java
@@ -2,6 +2,10 @@ package ca.mcgill.ecse321.GSSS.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ItemCategoryRepository {
+import ca.mcgill.ecse321.GSSS.model.ItemCategory;
+
+public interface ItemCategoryRepository extends CrudRepository <ItemCategory, String> {
+	
+	ItemCategory findItemCategoryByName(String name);
     
 }
