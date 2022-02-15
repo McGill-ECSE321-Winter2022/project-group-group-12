f1702c91a8efc8758f6e6c2d23f961711caf3df7
philippesarouphim <sarouphim.philippe@gmail.com>
2022-02-14 22:12:20 -0500

    Adapt existing tests

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/Employee.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/model/Employee.java
@@ -22,5 +22,24 @@ public class Employee extends Account {
   public void setShifts(Set<Shift> shifts) {
     this.shifts = shifts;
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Employee other = (Employee) obj;
+    if (shifts == null) {
+      if (other.shifts != null)
+        return false;
+    } else if (!shifts.equals(other.shifts))
+      return false;
+    return true;
+  }
+
+  
   
 }
