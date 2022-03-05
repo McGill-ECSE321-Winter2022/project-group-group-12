02cce018f84060ed148997f5af22ded74c0d72ac
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-03-05 12:20:46 -0500

    BusinessHour controller

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/dto/EmployeeDto.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/dto/EmployeeDto.java
@@ -2,20 +2,49 @@ package ca.mcgill.ecse321.GSSS.dto;
 
 import java.util.Set;
 
+/**
+ * DTO equivalent of the Employee class to pass to the frontend
+ * 
+ * @author Wassim Jabbour
+ *
+ */
 public class EmployeeDto extends AccountDto {
 
   private Set<ShiftDto> shifts;
 
+  /**
+   * Default constructor
+   * 
+   * @author Wassim Jabbour
+   */
   public EmployeeDto() {
 
   }
 
+  /**
+   * Full constructor
+   * 
+   * @author Wassim Jabbour
+   * @param username The name of the employee
+   * @param email The email of the employee
+   * @param password The password of the employee (hashed and salted)
+   * @param salt The salt used for the above password
+   * @param disabled Whether the employee is disabled
+   * @param address The address of the employee
+   * @param shifts The shifts of the employee
+   */
   public EmployeeDto(String username, String email, String password, String salt, boolean disabled,
       AddressDto address, Set<ShiftDto> shifts) {
     super(username, email, password, salt, disabled, address);
     this.shifts = shifts;
   }
 
+  /**
+   * Shifts getter
+   * 
+   * @author Wassim Jabbour
+   * @return The shifts of the employee
+   */
   public Set<ShiftDto> getShifts() {
     return shifts;
   }
