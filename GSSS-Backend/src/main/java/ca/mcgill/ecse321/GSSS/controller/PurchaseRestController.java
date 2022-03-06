99bd628c15311a54a7d45163835c78b09422e722
HabibJarweh <89218041+HabibJarweh@users.noreply.github.com>
2022-03-05 20:18:56 -0500

    did purchase getters and delete method

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/controller/PurchaseRestController.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/controller/PurchaseRestController.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse321.GSSS.controller;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +18,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ca.mcgill.ecse321.GSSS.dto.ItemDto;
 import ca.mcgill.ecse321.GSSS.dto.PurchaseDto;
+import ca.mcgill.ecse321.GSSS.model.Customer;
 import ca.mcgill.ecse321.GSSS.model.Employee;
 import ca.mcgill.ecse321.GSSS.model.Item;
 import ca.mcgill.ecse321.GSSS.model.OrderStatus;
 import ca.mcgill.ecse321.GSSS.model.OrderType;
 import ca.mcgill.ecse321.GSSS.model.Purchase;
+import ca.mcgill.ecse321.GSSS.service.CustomerService;
 import ca.mcgill.ecse321.GSSS.service.EmployeeService;
 import ca.mcgill.ecse321.GSSS.service.PurchaseService;
 
@@ -31,6 +37,99 @@ public class PurchaseRestController {
 
   @Autowired
   EmployeeService employeeService;
+
+  @Autowired
+  CustomerService customerService;
+
+  /**
+   * method to get purchase by id
+   * 
+   * @author Habib Jarweh
+   * @param id
+   * @return purchaseDto converted purchase
+   */
+  @GetMapping(value = {"/purchase/id", "/purchase/id/"})
+  public PurchaseDto getPurchaseById(@PathVariable("id") String id)
+      throws IllegalArgumentException {
+    Purchase purchase = purchaseService.getPurchase(id);
+    return DtoConversion.convertToDto(purchase);
+  }
+
+  /**
+   * method to get list of purchases by employee email
+   * 
+   * @author Habib Jarweh
+   * @param employeeEmail
+   * @return list of purchaseDto
+   */
+  @GetMapping(value = {"/purchases/{employeeEmail}", "/purchases/{employeeEmail}/"})
+  public List<PurchaseDto> getPurchasesByEmployee(
+      @PathVariable("employeeEmail") String employeeEmail) throws IllegalArgumentException {
+
+    Employee employee = employeeService.getEmployee(employeeEmail);
+    List<Purchase> allPurchases = purchaseService.getPurchasesByEmployee(employee);
+    List<PurchaseDto> purchaseDtos = new ArrayList<PurchaseDto>();
+
+    for (Purchase purchase : allPurchases) {
+
+      purchaseDtos.add(DtoConversion.convertToDto(purchase));
+
+    }
+    return purchaseDtos;
+
+  }
+
+  /**
+   * method to get list of purchases by date
+   * 
+   * @author Habib Jarweh
+   * @param date of purchases
+   * @return list of purchaseDto
+   */
+  @GetMapping(value = {"/purchases", "/purchases/"})
+  public List<PurchaseDto> getPurchasesByDate(Date date) throws IllegalArgumentException {
+
+    List<Purchase> allPurchases = purchaseService.getPurchasesByDate(date);
+    List<PurchaseDto> purchaseDtos = new ArrayList<PurchaseDto>();
+
+    for (Purchase purchase : allPurchases) {
+
+      purchaseDtos.add(DtoConversion.convertToDto(purchase));
+
+    }
+    return purchaseDtos;
+
+  }
+
+  /**
+   * method to get sorted list of purchases by customer email
+   * 
+   * @author Habib Jarweh
+   * @param customerEmail
+   * @return list of purchaseDto
+   */
+  @GetMapping(value = {"/purchases/{customerEmail}", "/purchases/{customerEmail}"})
+  public List<PurchaseDto> getPurchasesByCustomer(
+      @PathVariable("customerEmail") String customerEmail) throws IllegalArgumentException {
+
+    Customer customer = customerService.getCustomer(customerEmail);
+    List<Purchase> allPurchases = purchaseService.getOrderHistory(customer);
+    List<PurchaseDto> purchaseDtos = new ArrayList<PurchaseDto>();
+
+    Collections.sort(allPurchases, new Comparator<Purchase>() {
+      public int compare(Purchase p1, Purchase p2) {
+        return Long.valueOf(p2.getDate().getTime()).compareTo(p1.getDate().getTime());
+      }
+    });
+
+    for (Purchase purchase : allPurchases) {
+
+      purchaseDtos.add(DtoConversion.convertToDto(purchase));
+
+    }
+    return purchaseDtos;
+
+  }
 
   /**
    * Returns all purchases
@@ -109,9 +208,9 @@ public class PurchaseRestController {
       @PathVariable(name = "orderstatus") String orderStatus) throws IllegalArgumentException {
 
     OrderStatus actualOrderStatus = DtoConversion.findOrderStatusByName(orderStatus);
-    
+
     Purchase purchase = purchaseService.modifyPurchaseStatus(actualOrderStatus, purchaseId);
-    
+
     return DtoConversion.convertToDto(purchase);
   }
 
@@ -134,6 +233,18 @@ public class PurchaseRestController {
     Purchase purchase = purchaseService.modifyPurchaseEmployee(employee, purchaseId);
 
     return DtoConversion.convertToDto(purchase);
+  }
+
+  /**
+   * method to delete purchase
+   * 
+   * @author Habib Jarweh
+   * @param id id of purchase
+   * @throws IllegalArgumentException
+   */
+  @DeleteMapping(value = {"/item/{id}", "/item/{id}/"})
+  public void deletePurchase(@PathVariable("id") String id) throws IllegalArgumentException {
+    purchaseService.deletePurchase(id);
   }
 
 }
