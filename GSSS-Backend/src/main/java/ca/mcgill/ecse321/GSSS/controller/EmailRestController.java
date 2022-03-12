3efb704f4c7dc356f36ebecc20454516a80f8c46
HabibJarweh <89218041+HabibJarweh@users.noreply.github.com>
2022-03-12 12:04:57 -0500

    do send email controller

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/controller/EmailRestController.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/controller/EmailRestController.java
@@ -19,66 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RestController
 public class EmailRestController {
-
-
-
-//  public static void sendEmail(String email, String username) {
-//    
-//    String host    = "tls://smtp.gmail.com";
-//    int port    = 587;
-//    boolean debug   = true;
-//
-//    String password = "Beatles2022!";
-//
-//    String senderEmail = "beatlesofgsss@gmail.com";
-//
-////    String host = "localhost";
-//    
-//    // Set Properties
-//    Properties props = new Properties();
-//    
-//    props.put( "mail.smtp.auth", "true" );  
-//    props.put( "mail.smtp.host", host );
-//    props.put( "mail.smtp.port", port );
-//    props.put( "mail.smtp.starttls.enable", "true" ); 
-//    props.put( "mail.debug", debug );
-//    props.put( "mail.smtp.socketFactory.port", port );
-//    //props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
-//    props.put( "mail.smtp.socketFactory.fallback", "false" );
-//    props.put( "mail.smtp.ssl.trust", host ); 
-//    
-////    Properties properties = System.getProperties();
-////    properties.setProperty("mail.smtp.host", host);
-////    Session session = Session.getDefaultInstance(props);
-//    
-//    Session session = Session.getDefaultInstance(
-//        props,
-//        new javax.mail.Authenticator() {
-//
-//            protected PasswordAuthentication getPasswordAuthentication() {
-//
-//                return new PasswordAuthentication( username, password );
-//            }
-//        }
-//    );
-//
-//    try {
-//      // Create a default MimeMessage object.
-//      MimeMessage message = new MimeMessage(session);
-//      // Set From: header field of the header.
-//      message.setFrom(new InternetAddress(senderEmail));
-//      // Set To: header field of the header.
-//      message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-//      // Set Subject: header field
-//      message.setSubject("Set Password");
-//      // Now set the actual message
-//      message.setText("Hello " + username + ",\n"+"Please use this link to set your password");
-//      // Send message
-//      Transport.send(message);
-//    } catch (MessagingException mex) {
-//      mex.printStackTrace();
-//    }
-//  }
   
   @RequestMapping(value = "/sendemail")
   public static String sendEmail(String email, String username) throws AddressException, MessagingException, IOException {
@@ -103,7 +43,6 @@ public class EmailRestController {
 
     msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
     msg.setSubject("Set Password");
-//    msg.setContent("Tutorials point email", "text/html");
     msg.setText("Hello " + username + ",\n"+"Please use this link to set your password");
 //    msg.setSentDate(new Date());
 
