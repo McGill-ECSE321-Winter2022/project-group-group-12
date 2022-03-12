453c90ef48f750f4b6a49f347543fd7f085c605f
philippesarouphim <sarouphim.philippe@gmail.com>
2022-03-12 11:42:47 -0500

    Javadoc account service

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/service/AccountService.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/service/AccountService.java
@@ -17,10 +17,8 @@ import ca.mcgill.ecse321.GSSS.model.Account;
 import ca.mcgill.ecse321.GSSS.model.Customer;
 import ca.mcgill.ecse321.GSSS.model.Employee;
 import ca.mcgill.ecse321.GSSS.model.Owner;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
 
 
 @Service
@@ -95,6 +93,14 @@ public class AccountService {
 	    
 	}
 
+	/**
+	 * This method generates a JWT associated to an account and gives it appropriate permission.
+	 * 
+	 * @author Philippe Sarouphim Hochar.
+	 * 
+	 * @param account Account to generate JWT for.
+	 * @return The generated JWT.
+	 */
 	public String generateJWT(Account account){
 		Map<String, Object> claims = new HashMap<String, Object>();
 		if(account instanceof Owner) claims.put(PERMISSIONS_CLAIM_KEY, PERMISSIONS_OWNER);
@@ -104,21 +110,49 @@ public class AccountService {
 		return doGenerateToken(claims, account.getEmail());
 	}
 
+	/**
+	 * This method is a helper method which performs the generation of the JWT given payloads and an email.
+	 * 
+	 * @author Philippe Sarouphim Hochar.
+	 * 
+	 * @param claims Payload key-value pairs.
+	 * @param subject Email associated to the account.
+	 * @return The generated JWT.
+	 */
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
 		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
 				.signWith(SignatureAlgorithm.HS512, secret).compact();
 	}
 
+	/**
+	 * This methods verify whether the JWT is valid and if it's signature is valid.
+	 * 
+	 * @author Philippe Sarouphim Hochar.
+	 * 
+	 * @param token JWT.
+	 * @return Whether the token is valid or not.
+	 */
 	public boolean validateToken(String token) {
 		try{
-			Jwts.parser().parse(token);
+			Jwts.parser()
+			.setSigningKey(secret)
+			.parseClaimsJws(token);
 		} catch(Exception e){
 			return false;
 		}
 		return false;
 	}
 
+	/**
+	 * This method extracts the "permissions" field from the body of the JWT and returns it.
+	 * It only does so if the JWT is valid.
+	 * 
+	 * @author Philippe Sarouphim Hochar.
+	 * 
+	 * @param token JWT.
+	 * @return The permission associated to the JWT.
+	 */
 	public String getPermission(String token){
 		if(!validateToken(token)) return "none";
 		String permission = (String) Jwts.parser()
@@ -130,14 +164,38 @@ public class AccountService {
 		return permission;
 	}
 
+	/**
+	 * This method verifies whether the JWT has owner permissions.
+	 * 
+	 * @author Philippe Sarouphim Hochar.
+	 * 
+	 * @param token JWT.
+	 * @return Whether the JWT has owner permissions.
+	 */
 	public boolean getOwnerPermission(String token){
 		return getPermission(token).equals(PERMISSIONS_OWNER);
 	}
 
+	/**
+	 * This method verifies whether the JWT has employee permissions.
+	 * 
+	 * @author Philippe Sarouphim Hochar.
+	 * 
+	 * @param token JWT.
+	 * @return Whether the JWT has employee permissions.
+	 */
 	public boolean getEmployeePermission(String token){
 		return getPermission(token).equals(PERMISSIONS_EMPLOYEE);
 	}
 
+	/**
+	 * This method verifies whether the JWT has customer permissions.
+	 * 
+	 * @author Philippe Sarouphim Hochar.
+	 * 
+	 * @param token JWT.
+	 * @return Whether the JWT has customer permissions.
+	 */
 	public boolean getCustomerPermission(String token){
 		return getPermission(token).equals(PERMISSIONS_CUSTOMER);
 	}
