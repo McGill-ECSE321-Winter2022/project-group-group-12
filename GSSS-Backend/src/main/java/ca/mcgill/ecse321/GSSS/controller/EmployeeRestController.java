47c9c18172d9dc00b17a4d915f992867490923f7
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-03-15 19:10:39 -0400

    Revert "Javadoc"
    This reverts commit d42d921b2ac0bd84dc0e30dd50eed3296805dcb1.

--- GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/controller/EmployeeRestController.java
+++ GSSS-Backend/src/main/java/ca/mcgill/ecse321/GSSS/controller/EmployeeRestController.java
@@ -6,18 +6,12 @@ import ca.mcgill.ecse321.GSSS.model.Shift;
 import ca.mcgill.ecse321.GSSS.service.AddressService;
 import ca.mcgill.ecse321.GSSS.service.EmployeeService;
 import ca.mcgill.ecse321.GSSS.service.ShiftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * This Rest Controller class exposes the API endpoints related to employees.
@@ -28,20 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeRestController {
 
-  @Autowired
-  private EmployeeService employeeService;
+  @Autowired private EmployeeService employeeService;
 
-  @Autowired
-  private ShiftService shiftService;
+  @Autowired private ShiftService shiftService;
 
-  @Autowired
-  private AddressService addressService;
+  @Autowired private AddressService addressService;
 
   /**
    * This API endpoint fetches employees based on the query parameters.
    *
-   * @return A list of employees corresponding to the query.
    * @author Philippe Sarouphim Hochar.
+   * @return A list of employees corresponding to the query.
    */
   @GetMapping(value = {"/employees", "/employees/"})
   public List<EmployeeDto> getAllEmployees() {
@@ -53,10 +44,10 @@ public class EmployeeRestController {
   /**
    * This API endpoint creates a new employee.
    *
+   * @author Philippe Sarouphim Hochar.
    * @param employee Employee DTO (passed in the request body).
    * @return DTO of the newly created employee.
    * @throws IllegalArgumentException If the input is invalid
-   * @author Philippe Sarouphim Hochar.
    */
   @PostMapping(value = {"/employee", "/employee/"})
   public EmployeeDto createEmployee(@RequestBody EmployeeDto employee)
@@ -72,10 +63,10 @@ public class EmployeeRestController {
   /**
    * This API endpoint updates an employee.
    *
+   * @author Philippe Sarouphim Hochar.
    * @param employee Employee DTO
    * @return DTO of the newly updated employee.
    * @throws IllegalArgumentException If the input is invalid
-   * @author Philippe Sarouphim Hochar.
    */
   @PutMapping(value = {"/employee", "/employee/"})
   public EmployeeDto modifyEmployee(@RequestBody EmployeeDto employee)
@@ -92,11 +83,11 @@ public class EmployeeRestController {
   /**
    * This API endpoint gets and employee based on his email.
    *
+   * @author Philippe Sarouphim Hochar.
    * @param email Email of the employee to fetch.
    * @return DTO of the employee corresponding to the email.
    * @throws IllegalArgumentException If the input is invalid
-   * @throws NoSuchElementException   If the service method finds an element doesn't exist
-   * @author Philippe Sarouphim Hochar.
+   * @throws NoSuchElementException If the service method finds an element doesn't exist
    */
   @GetMapping(value = {"/employee/{email}", "/employee/{email}/"})
   public EmployeeDto getEmployee(@PathVariable("email") String email)
@@ -107,9 +98,9 @@ public class EmployeeRestController {
   /**
    * This API endpoint deletes an employee based on his email.
    *
+   * @author Philippe Sarouphim Hochar.
    * @param email Email of the employee to delete.
    * @throws IllegalArgumentException If the input is invalid
-   * @author Philippe Sarouphim Hochar.
    */
   @DeleteMapping(value = {"/employee/{email}", "/employee/{email}/"})
   public void deleteEmployee(@PathVariable("email") String email) throws IllegalArgumentException {
@@ -121,12 +112,12 @@ public class EmployeeRestController {
    * because this method takes care of creating the shifts anyways (You cannot have a shift without
    * an employee so might as well create both in one go)
    *
+   * @author Philippe Sarouphim Hochar.
    * @param email Email of the employee (in the path).
    * @param shift Shift (in the req body).
    * @return DTO of the new employee.
    * @throws IllegalArgumentException If the input is invalid
-   * @throws NoSuchElementException   If the service method finds an element doesn't exist
-   * @author Philippe Sarouphim Hochar.
+   * @throws NoSuchElementException If the service method finds an element doesn't exist
    */
   @PostMapping(value = {"/employee/shift/{email}", "/employee/shift/{email}/"})
   public EmployeeDto addShift(@PathVariable String email, @RequestBody ShiftDto shift)
@@ -140,10 +131,10 @@ public class EmployeeRestController {
   /**
    * This API endpoint removes a shift from an employee.
    *
-   * @param email Email of the employee (passed in path).
-   * @param id    Id of the shift (passed in path).
-   * @throws IllegalArgumentException If the input is invalid
    * @author Philippe Sarouphim Hochar.
+   * @param email Email of the employee (passed in path).
+   * @param id Id of the shift (passed in path).
+   * @throws IllegalArgumentException If the input is invalid
    */
   @DeleteMapping(value = {"/employee/shift/{email}/{id}", "/employee/shift/{email}/{id}/"})
   public void removeShift(@PathVariable("email") String email, @PathVariable("id") String id)
@@ -151,8 +142,4 @@ public class EmployeeRestController {
     employeeService.removeShift(
         employeeService.getEmployeeByEmail(email), shiftService.getShift(id));
   }
-
-  /**
-   * No need for a get shifts method since we can get the employee by email and the shifts are returned in his DTO
-   */
 }
