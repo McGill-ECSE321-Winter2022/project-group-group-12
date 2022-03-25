af53c4a120a250acc8a933bb1bdc00ea70133b6b
HabibJarweh <89218041+HabibJarweh@users.noreply.github.com>
2022-03-25 19:25:06 -0400

    add sign up page

---
+++ GSSS-Frontend/src/components/SignupPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="signup-container">
+        <div>
+            <h1>Sign Up</h1>
+            <div>Full Name:</div>
+            <input placeholder="Enter your full name" type="text" />
+            <div>Email:</div>
+            <input placeholder="Enter email address" type="text" />
+            <div>Username:</div>
+            <input placeholder="Enter username" type="text" />
+            <div>Password:</div>
+            <input placeholder="Enter password" type="password" />
+            <div>Address:</div>
+            <input placeholder="Enter street name" type="text" />
+            <input placeholder="Enter street number" type="text" />
+            <input placeholder="Enter city" type="text" />
+            <input placeholder="Enter postal code" type="text" />
+
+            <button>Sign Up</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    @import '../assets/styles/signup.css';
+</style>
+
+<script>
+export default{
+    name: 'SignupPage',
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
