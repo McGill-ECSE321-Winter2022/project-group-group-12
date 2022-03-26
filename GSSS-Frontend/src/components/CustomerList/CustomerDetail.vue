f9692f0df9f33e69b27d8f333849ba08f23e010f
philippesarouphim <sarouphim.philippe@gmail.com>
2022-03-26 16:05:01 -0400

    Add support for address in customer list

--- GSSS-Frontend/src/components/CustomerList/CustomerDetail.vue
+++ GSSS-Frontend/src/components/CustomerList/CustomerDetail.vue
@@ -2,7 +2,9 @@
     <div>
         <h3>{{ customer.email }}</h3>
         <div>Username: {{ customer.username }}</div>
-        <div>Disabled: {{ customer.disabled }}</div>
+        <div style="margin-bottom: 1rem">Disabled: {{ customer.disabled }}</div>
+        <div>Address: ({{ customer.address.id }})</div>
+        <p>{{ customer.address.fullName }}<br/>{{customer.address.streetNumber}}, {{ customer.address.streetName }}<br/>{{ customer.address.city }}, {{ customer.address.postalCode }}</p>
     </div>
 </template>
 
