44202c330447da86957fd64d5c5d278f3b099df3
Theo-Ghanem <90062145+Theo-Ghanem@users.noreply.github.com>
2022-03-26 18:25:06 -0400

    Started making the purchase statistics page

--- GSSS-Frontend/src/components/PurchaseStatistics.vue
+++ GSSS-Frontend/src/components/PurchaseStatistics.vue
@@ -1,13 +1,25 @@
 <template>
-
+  <div>
+    <h1>Get Purchase Statistics by date</h1>
+    <br>
+    <p> Choose a date:</p>
+    <date-picker v-model="time1" valueType="format"></date-picker>
+  </div>
 </template>
 
+
 <script>
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
 export default {
-  name: "PurchaseStatistics"
-}
+  components: { DatePicker },
+  data() {
+    return {
+      time1: null
+
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
