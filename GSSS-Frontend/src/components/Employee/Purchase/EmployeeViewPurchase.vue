0a6d71d21371e8f0048fee9851b64acea4f06a67
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2022-03-30 17:45:39 -0400

    Update EmployeeViewPurchase.vue

--- GSSS-Frontend/src/components/Employee/Purchase/EmployeeViewPurchase.vue
+++ GSSS-Frontend/src/components/Employee/Purchase/EmployeeViewPurchase.vue
@@ -23,7 +23,107 @@
 
 </template>
 
-<script src="./purchase.js">
+<script>
+
+// Importing axios and setting up URLs
+import axios from 'axios'
+var config = require('../../../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+export default {
+
+  name: 'EmployeeViewPurchases',
+
+  data () {
+    return {
+      purchases: [],
+      selectedPurchase: -1, // The index of the selected purchase
+      error: '',
+      response: [],
+      selectedPurchaseItems: [],
+      selectedPurchaseQuantities: [],
+      selectedPurchaseItemsPrices: []
+    }
+  },
+
+  created: function() {
+
+    // Getting the purchases from the backend
+    AXIOS.get('purchasesbyemployee/' + employeeEmail)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.purchases = response.data
+
+      // Iterating over all purchases and adding their customer's email as a field
+      for(let i = 0; i < this.purchases.length; i++) {
+         AXIOS.get('purchasesbyemployee/' + employeeEmail)
+        .then(response => {
+          this.purchases[i] = response.data.email
+        })
+        .catch(e => {
+          this.error = e
+        })
+      }
+
+      // Iterating over all purchases and adding their cost as a field
+      for(let i = 0; i < this.purchases.length; i++) {
+        AXIOS.get('/purchase/cost/' + this.purchases[i].id)
+        .then(response => {
+          this.purchases[i].cost = response.data
+        })
+        .catch(e => {
+          this.error = e
+        })
+      }
+      
+      })
+    .catch(e => {
+      this.error = e
+    })
+  },
+
+  methods: {
+    
+    onPurchaseSelect: function(i) {
+      
+      // Set the selected purchase to be the one at index i
+      this.selectedPurchase = i
+
+
+      // Refresh the selectedPurchase item lists
+      this.selectedPurchaseItems = []
+      this.selectedPurchaseQuantities = []
+      this.selectedPurchaseItemsPrices = []
+      
+      for (const [item, quantity] of Object.entries(this.purchases[i].items)) {
+        this.selectedPurchaseItems.push(item)
+        this.selectedPurchaseQuantities.push(quantity)
+      }
+
+      for (let i = 0; i < this.selectedPurchaseItems.length; i++) {
+        AXIOS.get('/item/' + this.selectedPurchaseItems[i])
+        .then(response => {
+          this.selectedPurchaseItemsPrices.push(response.data.price)
+        })
+        .catch(e => {
+          this.error = e
+        })
+      }
+
+
+    }
+
+  },
+
+
+  }
 </script>
 
 <style scoped>
