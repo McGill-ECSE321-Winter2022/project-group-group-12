9a761e0ba834741db6b062dcac7ace57cbaf6b08
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-03-30 14:44:46 -0400

    View all purchases backend works!

--- GSSS-Frontend/src/components/Owner/ViewPurchases/ViewPurchases.vue
+++ GSSS-Frontend/src/components/Owner/ViewPurchases/ViewPurchases.vue
@@ -11,16 +11,17 @@
     <div class="details">
         <div v-if="this.selectedPurchase!=-1">
           <h3>{{ purchases[selectedPurchase].date }}</h3>
-          <div>Id: {{ purchases[selectedPurchase].id }}</div>
-          <div style="margin-bottom: 1rem">Customer email:{{ purchases[selectedPurchase].customer }}</div>
+          <div> Id: {{ purchases[selectedPurchase].id }}</div>
+          <div> Customer email: {{ purchases[selectedPurchase].customer }}</div>
           <div> Order Type: {{ purchases[selectedPurchase].orderType }}</div>
           <div> Order status: {{ purchases[selectedPurchase].orderStatus }}</div>
           <div> Date: {{ purchases[selectedPurchase].date }}</div>
           <div> Time: {{ purchases[selectedPurchase].time }}</div>
           <div> Employee: {{ purchases[selectedPurchase].employee.email }} </div>
+          <div> Cost: {{ purchases[selectedPurchase].cost }}$</div>
           <div> Items:
               <ul v-for="(n, index) in this.selectedPurchaseItems.length" :key="index"> 
-                  <li> {{ selectedPurchaseItems[index] }} : {{ selectedPurchaseQuantities[index] }}</li>
+                  <li> {{ selectedPurchaseItems[index] }} : {{ selectedPurchaseQuantities[index] }} ( {{ selectedPurchaseItemsPrices[index] }}$ / unit )</li>
               </ul>
           </div>
         </div>
@@ -29,9 +30,6 @@
 </template>
 
 <script>
-
-// Importing components
-import PurchaseDetails from './PurchaseDetails'
 
 // Importing axios and setting up URLs
 import axios from 'axios'
@@ -56,31 +54,43 @@ export default {
       error: '',
       response: [],
       selectedPurchaseItems: [],
-      selectedPurchaseQuantities: []
+      selectedPurchaseQuantities: [],
+      selectedPurchaseItemsPrices: []
     }
   },
 
   created: function() {
-  
+
     // Getting the purchases from the backend
     AXIOS.get('/purchases')
     .then(response => {
-
+      
       // JSON responses are automatically parsed.
       this.purchases = response.data
 
-
       // Iterating over all purchases and adding their customer's email as a field
-      for(purchase in this.purchases) {
-        AXIOS.get('/customerByPurchase' + purchase.id)
+      for(let i = 0; i < this.purchases.length; i++) {
+        AXIOS.get('/customerByPurchase/' + this.purchases[i].id)
         .then(response => {
-          purchase.customer = response.data.email
+          this.purchases[i].customer = response.data.email
         })
         .catch(e => {
           this.error = e
         })
       }
-    })
+
+      // Iterating over all purchases and adding their cost as a field
+      for(let i = 0; i < this.purchases.length; i++) {
+        AXIOS.get('/purchase/cost/' + this.purchases[i].id)
+        .then(response => {
+          this.purchases[i].cost = response.data
+        })
+        .catch(e => {
+          this.error = e
+        })
+      }
+      
+      })
     .catch(e => {
       this.error = e
     })
@@ -93,16 +103,30 @@ export default {
       // Set the selected purchase to be the one at index i
       this.selectedPurchase = i
 
+
       // Refresh the selectedPurchase item lists
       this.selectedPurchaseItems = []
       this.selectedPurchaseQuantities = []
+      this.selectedPurchaseItemsPrices = []
       
       for (const [item, quantity] of Object.entries(this.purchases[i].items)) {
-        console.log(item)
         this.selectedPurchaseItems.push(item)
         this.selectedPurchaseQuantities.push(quantity)
       }
+
+      for (let i = 0; i < this.selectedPurchaseItems.length; i++) {
+        AXIOS.get('/item/' + this.selectedPurchaseItems[i])
+        .then(response => {
+          this.selectedPurchaseItemsPrices.push(response.data.price)
+        })
+        .catch(e => {
+          this.error = e
+        })
+      }
+
+
     }
+
   },
 
 
