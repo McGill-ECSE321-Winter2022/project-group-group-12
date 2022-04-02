fd0573bfc4f4fe6989c1bb993b8f28a44e698754
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-04-02 14:48:45 -0400

    Fix login page error placement

--- GSSS-Frontend/src/components/LoginPage/LoginPage.vue
+++ GSSS-Frontend/src/components/LoginPage/LoginPage.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="login-container">
-        <div>
-            <h1>Login</h1>
-            <div>Email:</div>
-            <input v-model="email" placeholder="Enter email" type="text" />
-            <div>Password:</div>
-            <input v-model="password" placeholder="Enter password" type="password" />
-            <button @click="logIn()">Login</button>
-            <div v-if="error" class="error">
-                <div>
-                    {{ error }}
-                </div>
+    <div class="container">
+        <div class="login-container">
+            <div>
+                <h1>Login</h1>
+                <div>Email:</div>
+                <input v-model="email" placeholder="Enter email" type="text" />
+                <div>Password:</div>
+                <input v-model="password" placeholder="Enter password" type="password" />
+                <button @click="logIn()">Login</button>
+            </div>
+        </div>
+        <div v-if="error" class="error">
+            <div>
+                {{ error }}
             </div>
         </div>
     </div>
