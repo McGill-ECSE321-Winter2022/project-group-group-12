02ba6cbc1eda4d426d5d6da0507263183ad3dc04
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-04-09 00:22:33 -0400

    Fix redirection problem

--- GSSS-Android/app/src/main/java/ca/mcgill/ecse321/GSSS/MainActivity.java
+++ GSSS-Android/app/src/main/java/ca/mcgill/ecse321/GSSS/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient()); // To open URLs within WebView
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
