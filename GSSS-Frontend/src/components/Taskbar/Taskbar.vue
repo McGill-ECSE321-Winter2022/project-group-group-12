e336b6a92d33108ae16a87b88634957213968bf3
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-04-09 20:05:41 -0400

    Fix taskbar!!!

--- GSSS-Frontend/src/components/Taskbar/Taskbar.vue
+++ GSSS-Frontend/src/components/Taskbar/Taskbar.vue
@@ -6,15 +6,15 @@
     <header>
             <a href="/"><img src="../../assets/Logo_GSSS_notext.png" alt="centered image" height="60px" width="60px"></a>
             <h1><a href="/">GSSS</a></h1>
-            <div>
+            <div v-bind:style="taskbar? '--open:auto': '--open:none'">
                 <!-- Links if no user is logged in -->
-                <nav v-if="permission == -1">
+                <nav v-if="permission == -1"  v-on:click="taskbar=false;">
                     <a href="/#/login">Login</a>
                     <a href="/#/signup">Signup</a>
                 </nav>
 
                 <!-- Links if a customer is logged in -->
-                <nav v-if="permission == 0">
+                <nav v-if="permission == 0"  v-on:click="taskbar=false;">
                     <a href="/#/customer/account">Account info</a>
                     <a href="/#/customer/shop">Shop</a>
                     <a href='/#/customer/storeinformation'>Store information</a>
@@ -23,25 +23,17 @@
                 </nav>
 
                 <!-- Links if an employee is logged in -->
-                <nav v-if="permission == 1">
+                <nav v-if="permission == 1"  v-on:click="taskbar=false;">
                     <a href="/#/employee/account">Account info</a>
                     <a href="/#/employee/customers">Customers</a>
                     <a href='/#/employee/purchases'>Assigned purchases</a>
                     <a href='/#/employee/view/shifts'>Assigned shifts</a>
                     <a href='/#/customer/storeinformation'>Store information</a>
-                    <select name="list" id="list" v-on:change="goToNewPage()">
-                        <option value='/' selected>Home</option>
-                        <option value="/#/employee/customers">Account info</option>
-                        <option value="/#/employee/customers">Customers</option>
-                        <option value="/#/employee/purchases">Assigned purchases</option>
-                        <option value="/#/employee/view/shifts">Assigned shifts</option>
-                        <option value="/#/customer/storeinformation">Store information</option>
-                    </select>
                     <a v-on:click="logout()">Logout</a>
                 </nav>
 
                 <!-- Links if an owner is logged in -->
-                <nav v-if="permission == 2">
+                <nav v-if="permission == 2" v-on:click="taskbar=false;">
                     <a href="/#/owner/systeminformation">System Information</a>
                     <a href="/#/owner/employees">Employees</a>
                     <a href="/#/owner/purchases">Purchases</a>
@@ -51,6 +43,11 @@
                     <a href='/#/employee/customers'>Customers</a>
                     <a v-on:click="logout()">Logout</a>
                 </nav>
+
+                <button class="taskbar-button" v-on:click="toggleTaskbar()">
+                    <img v-if="!taskbar" style="display:block;" src="../../assets/menu-icon.jpg" height="40px">
+                    <img v-if="taskbar" style="display:block;" src="../../assets/close-icon.svg" height="30px">
+                </button>
             </div>
     </header>
 </template>
--- GSSS-Frontend/src/components/Taskbar/taskbar.js
+++ GSSS-Frontend/src/components/Taskbar/taskbar.js
@@ -5,13 +5,6 @@ function getPermission(){
     return -1;
 }
 
-// function getOpt(period) {
-//     if (period.value = "Accountinfo") { document.getElementById("abc").href="/#/employee/account"; }
-//     else if (period.value = "Customers") { document.getElementById("abc").href="/#/employee/customers"; }
-//     else { document.getElementById("abc").href="/#/employee/purchases"; } 
-//     }
-
-
 const permissions = {
     None: -1,
     Customer: 0,
@@ -23,7 +16,8 @@ export default {
     name: 'taskbar',
     data: function(){
         return {
-            permission: -1
+            permission: -1,
+            taskbar: false
         }
     },
 
@@ -36,13 +30,10 @@ export default {
             logOut();
             this.$router.push( { name: 'LoginPage' } );
         },
-        goToNewPage: function()
-        {
-            var url = document.getElementById('list').value;
-            if(url != 'none') {
-            window.location = url;
+
+        toggleTaskbar: function() {
+            this.taskbar = !this.taskbar;
         }
-    }
     },
 
     watch: {
