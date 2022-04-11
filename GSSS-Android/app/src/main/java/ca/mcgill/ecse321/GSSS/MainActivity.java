cf032f2d72ca88876f9187f4adafde782aed398c
Wassim Jabbour <wassim.jabbour@mail.mcgill.ca>
2022-04-11 14:30:10 -0400

    Remove unused lines and comment main activity

--- GSSS-Android/app/src/main/java/ca/mcgill/ecse321/GSSS/MainActivity.java
+++ GSSS-Android/app/src/main/java/ca/mcgill/ecse321/GSSS/MainActivity.java
@@ -8,17 +8,24 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    // The webview of the main activity
     private WebView webView;
 
+    /**
+     * Method that runs when the app is opened
+     * @param savedInstanceState To load the last state of the app
+     * @author Wassim Jabbour
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webView);
+        super.onCreate(savedInstanceState); // Call superclass constructor
+        setContentView(R.layout.activity_main); // Link to XML file
+        webView = (WebView) findViewById(R.id.webView); // Find the webview by ID
         webView.setWebViewClient(new WebViewClient()); // To open URLs within WebView
         WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webView.loadUrl("https://grocerystore-frontend.herokuapp.com");
+        webSettings.setJavaScriptEnabled(true); // Enable JS
+        webSettings.setDomStorageEnabled(true); // Enable DOM storage
+        webView.loadUrl("https://grocerystore-frontend.herokuapp.com"); // Load our webapp
     }
 }
